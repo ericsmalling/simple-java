6c31ccd978d5bad88d8badada8fda3b690acf5a6
Eric Smalling <github@ericsmalling.com>
2015-04-10 10:00:07 -0500

    initial commit

---
+++ src/main/java/com/ericsmalling/App.java
@@ -0,0 +1,13 @@
+package com.ericsmalling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
